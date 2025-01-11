73bb38869982a857f1e8b61c48e5a34f4a6461d8
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2025-01-12 00:59:13 +0500

    added Profile component with user data fetching and integrated UpdateProfile for updates

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,29 +1,72 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useState } from "react";
 import { useNavigate } from "react-router";
 import { NavBar } from "../components/Navbar";
 import authApi from "../api/authApi";
 import { useEffect } from "react";
 import { useRoleContext } from "../context/roleContext";
+import toast from "react-hot-toast";
+import { UpdateProfile } from "../components/UpdateProfile";
 
 function Profile() {
+  const [userData, setUserData] = useState({
+    username: "",
+    name: "",
+    email: "",
+    gender: "",
+    address: "",
+    image: "",
+  });
+  const [userFullData, setUserFullData] = useState({});
   const navigate = useNavigate();
   const { setStatus, setRole, status } = useRoleContext();
   useEffect(() => {
     authApi.verifyLogin().then(async (response) => {
       if (!response) {
         setStatus(response);
+        navigate("/");
       } else {
         setStatus(response.status);
         setRole(response.role);
+        const data = await authApi.getData();
+        if (data.success) {
+          setUserData({
+            username: data.user.username,
+            name: `${data.user.firstName} ${data.user.lastName}`,
+            email: data.user.email,
+            gender: data.user.gender === "male" ? "Male" : "Female",
+            address: data.user.address,
+            image: data.user.profileImage,
+          });
+          setUserFullData(data.user);
+        } else {
+          toast.error(data.error);
+        }
       }
     });
-    if (!status) {
-      navigate("/");
-    }
-  });
+  }, [status]);
 
   return (
     <>
       <NavBar />
+      <div>
+        <h1>Profile</h1>
+        <img
+          src={userData.image}
+          alt="profile"
+          className="h-28 w-28 rounded-full"
+        />
+        <div>Name : {userData.name}</div>
+        <div>Username : {userData.username}</div>
+        <div>Email : {userData.email}</div>
+        <div>Gender : {userData.gender}</div>
+        <div>Address : {userData.address}</div>
+      </div>
+      <UpdateProfile
+        userData={userFullData}
+        setNewData={setUserFullData}
+        setUserData={setUserData}
+      />
     </>
   );
 }
