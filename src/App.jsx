57bbc1eae915259fa1f02a5482c28addceccf53a
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2025-01-11 23:41:19 +0500

    refactored App component to use a main tag for improved semantic structure

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import AppRouter from "./router";
 
 function App() {
   return (
-    <>
+    <main className="min-h-screen min-w-[100vw] bg-sky-200 dark:bg-slate-950">
       <Toaster position="top-center" />
       <AppRouter />
-    </>
+    </main>
   );
 }
 
