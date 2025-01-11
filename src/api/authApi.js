fe16ca382a190dcbd844fb1e2a8ac5d1966af6b2
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2025-01-11 19:12:07 +0500

    added register function to authApi for user registration handling

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -52,10 +52,34 @@ async function logout() {
   return data;
 }
 
+async function register(credentials) {
+  const response = await fetch(`${api_url}/api/user/`, {
+    credentials: "include",
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(credentials),
+  });
+  const data = await response.json();
+  if (!data.success) {
+    if (!Array.isArray(data.error)) {
+      return data;
+    }
+    data.error = data.error
+      .map((error) => {
+        return error.msg;
+      })
+      .join(",");
+  }
+  return data;
+}
+
 const authApi = {
   login,
   verifyLogin,
   logout,
+  register,
 };
 
 export default authApi;
