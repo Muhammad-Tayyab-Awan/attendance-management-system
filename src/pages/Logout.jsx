ff4deb4fb764aa4253e2b680cc72bc352d7e0284
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2025-01-11 18:50:26 +0500

    added Logout component for user session termination and navigation

---
+++ src/pages/Logout.jsx
@@ -0,0 +1,51 @@
+import { useNavigate } from "react-router";
+import { NavBar } from "../components/Navbar";
+import authApi from "../api/authApi";
+import { useEffect } from "react";
+import { useRoleContext } from "../context/roleContext";
+import { Button } from "flowbite-react";
+import toast from "react-hot-toast";
+
+function Logout() {
+  const navigate = useNavigate();
+  const { setStatus, setRole, status } = useRoleContext();
+  useEffect(() => {
+    authApi.verifyLogin().then(async (response) => {
+      if (!response) {
+        setStatus(response);
+      } else {
+        setStatus(response.status);
+        setRole(response.role);
+      }
+    });
+    if (!status) {
+      navigate("/");
+    }
+  });
+
+  const handleSubmit = async () => {
+    authApi.logout().then((response) => {
+      if (response.success) {
+        toast.success(response.msg);
+        navigate("/");
+        setRole(null);
+        setStatus(false);
+      } else {
+        toast.error(response.error);
+        navigate("/");
+      }
+    });
+  };
+
+  return (
+    <>
+      <NavBar />
+      Are you confirm to logout?
+      <Button size="sm" onClick={handleSubmit}>
+        Logout
+      </Button>
+    </>
+  );
+}
+
+export default Logout;
