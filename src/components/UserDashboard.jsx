1525c726aba07c6bf653f6574eacb67e39a493bd
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2025-01-12 17:06:32 +0500

    added ViewAttendanceUser and ViewLeaveUser components to UserDashboard

--- src/components/UserDashboard.jsx
+++ src/components/UserDashboard.jsx
@@ -1,11 +1,15 @@
 import MarkAttendance from "./MarkAttendance";
 import SubmitLeave from "./SubmitLeave";
+import ViewAttendanceUser from "./ViewAttendanceUser";
+import ViewLeaveUser from "./ViewLeaveUser";
 
 function UserDashboard() {
   return (
     <div>
       <MarkAttendance />
       <SubmitLeave />
+      <ViewAttendanceUser />
+      <ViewLeaveUser />
     </div>
   );
 }
