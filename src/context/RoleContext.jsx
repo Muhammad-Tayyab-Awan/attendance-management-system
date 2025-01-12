fa9d1e1e3a440e66937efb59b38dc27f9fbc9486
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2025-01-12 19:17:14 +0500

    added login verification in RoleContext to manage user authentication status and role

--- src/context/RoleContext.jsx
+++ src/context/RoleContext.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react-refresh/only-export-components */
 /* eslint-disable react/prop-types */
-import { createContext, useState, useContext } from "react";
+import { createContext, useState, useContext, useEffect } from "react";
+import authApi from "../api/authApi";
 
 const RoleContext = createContext({
   status: false,
@@ -12,6 +13,18 @@ const RoleContext = createContext({
 export const RoleProvider = ({ children }) => {
   const [status, setStatus] = useState(false);
   const [role, setRole] = useState(null);
+
+  useEffect(() => {
+    authApi.verifyLogin().then((res) => {
+      if (res.status) {
+        setStatus(true);
+        setRole(res.role);
+      } else {
+        setStatus(res.status);
+        setRole(null);
+      }
+    });
+  }, []);
 
   return (
     <RoleContext.Provider value={{ status, role, setStatus, setRole }}>
