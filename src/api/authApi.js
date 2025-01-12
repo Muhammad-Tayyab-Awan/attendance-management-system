1591f0e6b00e99546eee9127a1a92b0796a6a724
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2025-01-12 23:08:19 +0500

    added addUser function in authApi for user registration

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -144,6 +144,29 @@ async function deleteImage() {
   return data;
 }
 
+async function addUser(credentials) {
+  const response = await fetch(`${api_url}/api/user/`, {
+    credentials: "include",
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(credentials),
+  });
+  const data = await response.json();
+  if (!data.success) {
+    if (!Array.isArray(data.error)) {
+      return data;
+    }
+    data.error = data.error
+      .map((error) => {
+        return error.msg;
+      })
+      .join(",");
+  }
+  return data;
+}
+
 const authApi = {
   login,
   verifyLogin,
@@ -154,6 +177,7 @@ const authApi = {
   deleteAccount,
   uploadImage,
   deleteImage,
+  addUser,
 };
 
 export default authApi;
