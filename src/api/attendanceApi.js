fcedcf475ad834756a76db1a4935ec4c97b3e36f
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2025-01-12 23:16:04 +0500

    added getAllAttendanceByAdmin function to attendanceApi for fetching all attendance records by admin

--- src/api/attendanceApi.js
+++ src/api/attendanceApi.js
@@ -36,10 +36,23 @@ async function getOverallAttendanceOfUser() {
   return data;
 }
 
+async function getAllAttendanceByAdmin() {
+  const response = await fetch(`${api_url}/api/attendance/all-admin`, {
+    method: "GET",
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const data = await response.json();
+  return data;
+}
+
 const attendanceApi = {
   getAttendance,
   markAttendance,
   getOverallAttendanceOfUser,
+  getAllAttendanceByAdmin,
 };
 
 export default attendanceApi;
