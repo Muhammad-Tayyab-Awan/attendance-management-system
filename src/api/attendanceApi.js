1ec0d626af5dc4ed0f0f7ce7012f0dd9de95bb89
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2025-01-12 15:49:07 +0500

    added getOverallAttendanceOfUser function to attendanceApi for fetching overall attendance data

--- src/api/attendanceApi.js
+++ src/api/attendanceApi.js
@@ -16,11 +16,30 @@ async function markAttendance() {
   const response = await fetch(`${api_url}/api/attendance`, {
     method: "POST",
     credentials: "include",
+    headers: {
+      "Content-Type": "application/json",
+    },
   });
   const data = await response.json();
   return data;
 }
 
-const attendanceApi = { getAttendance, markAttendance };
+async function getOverallAttendanceOfUser() {
+  const response = await fetch(`${api_url}/api/attendance/all`, {
+    method: "GET",
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const data = await response.json();
+  return data;
+}
+
+const attendanceApi = {
+  getAttendance,
+  markAttendance,
+  getOverallAttendanceOfUser,
+};
 
 export default attendanceApi;
