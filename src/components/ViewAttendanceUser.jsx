1ef11dd7718acb1cd4bdd1c7350f23964dcd0ad0
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2025-01-16 17:58:10 +0500

    refactor: replace CompactTable with Flowbite Table for improved attendance display

--- src/components/ViewAttendanceUser.jsx
+++ src/components/ViewAttendanceUser.jsx
@@ -1,67 +1,61 @@
 import * as React from "react";
-
-import { CompactTable } from "@table-library/react-table-library/compact";
-import { useTheme } from "@table-library/react-table-library/theme";
-import { getTheme } from "@table-library/react-table-library/baseline";
-
 import attendanceApi from "../api/attendanceApi";
 import toast from "react-hot-toast";
+import { Table } from "flowbite-react";
 
 const ViewAttendanceUser = () => {
-  const [data, setData] = React.useState({
-    nodes: [],
-  });
+  const [data, setData] = React.useState([]);
   React.useEffect(() => {
     attendanceApi.getOverallAttendanceOfUser().then((response) => {
       if (response.success) {
-        setData({ nodes: response.attendances });
+        setData(response.attendances);
       } else {
         toast.error(response.error);
       }
     });
   }, []);
 
-  const theme = useTheme([
-    getTheme(),
-    {
-      HeaderRow: `
-        background-color: #eaf5fd;
-      `,
-      Row: `
-        &:nth-of-type(odd) {
-          background-color: #d2e9fb;
-        }
-
-        &:nth-of-type(even) {
-          background-color: #eaf5fd;
-        }
-      `,
-    },
-  ]);
-
   function capitalizeFirstLetter(val) {
     return String(val).charAt(0).toUpperCase() + String(val).slice(1);
   }
 
-  const COLUMNS = [
-    { label: "Id", renderCell: (item) => item._id },
-    {
-      label: "Date",
-      renderCell: (item) => new Date(item.date).toUTCString(),
-    },
-    {
-      label: "Status",
-      renderCell: (item) => capitalizeFirstLetter(item.status),
-    },
-    {
-      label: "Marked on",
-      renderCell: (item) => new Date(item.markedDate).toUTCString(),
-    },
-  ];
-
   return (
     <>
-      <CompactTable columns={COLUMNS} data={data} theme={theme} />
+      {data && data.length > 0 ? (
+        <div className="my-4 overflow-x-auto">
+          <Table className="mx-auto w-auto">
+            <Table.Head>
+              <Table.HeadCell>Date</Table.HeadCell>
+              <Table.HeadCell>Status</Table.HeadCell>
+              <Table.HeadCell>Marked on</Table.HeadCell>
+            </Table.Head>
+            <Table.Body className="divide-y">
+              {data.map((attendance) => {
+                return (
+                  <Table.Row
+                    key={attendance._id}
+                    className="bg-slate-300 dark:border-gray-700 dark:bg-gray-800"
+                  >
+                    <Table.Cell>
+                      {new Date(attendance.date).toDateString()}
+                    </Table.Cell>
+                    <Table.Cell>
+                      {capitalizeFirstLetter(attendance.status)}
+                    </Table.Cell>
+                    <Table.Cell>
+                      {new Date(attendance.markedDate).toTimeString()}
+                    </Table.Cell>
+                  </Table.Row>
+                );
+              })}
+            </Table.Body>
+          </Table>
+        </div>
+      ) : (
+        <div className="mx-auto w-[80%] py-2 text-center text-xl">
+          No attendance record found
+        </div>
+      )}
     </>
   );
 };
