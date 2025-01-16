be846f10d436c46dc55cb1f98f1776f8978fc2ac
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2025-01-16 20:22:25 +0500

    refactor: replaced CompactTable with Flowbite Table for improved leave display

--- src/components/ViewLeaveUser.jsx
+++ src/components/ViewLeaveUser.jsx
@@ -1,71 +1,65 @@
 import * as React from "react";
-
-import { CompactTable } from "@table-library/react-table-library/compact";
-import { useTheme } from "@table-library/react-table-library/theme";
-import { getTheme } from "@table-library/react-table-library/baseline";
-
 import toast from "react-hot-toast";
 import leaveApi from "../api/leaveApi";
+import { Table } from "flowbite-react";
 
 const ViewLeaveUser = () => {
-  const [data, setData] = React.useState({
-    nodes: [],
-  });
+  const [data, setData] = React.useState([]);
   React.useEffect(() => {
     leaveApi.getOverallLeaveOfUser().then((response) => {
       if (response.success) {
-        setData({ nodes: response.allLeaves });
+        setData(response.allLeaves);
       } else {
         toast.error(response.error);
       }
     });
   }, []);
 
-  const theme = useTheme([
-    getTheme(),
-    {
-      HeaderRow: `
-        background-color: #eaf5fd;
-      `,
-      Row: `
-        &:nth-of-type(odd) {
-          background-color: #d2e9fb;
-        }
-
-        &:nth-of-type(even) {
-          background-color: #eaf5fd;
-        }
-      `,
-    },
-  ]);
-
   function capitalizeFirstLetter(val) {
     return String(val).charAt(0).toUpperCase() + String(val).slice(1);
   }
 
-  const COLUMNS = [
-    { label: "Id", renderCell: (item) => item._id },
-    {
-      label: "Starting From",
-      renderCell: (item) => new Date(item.startDate).toUTCString(),
-    },
-    {
-      label: "Ending on",
-      renderCell: (item) => new Date(item.endDate).toUTCString(),
-    },
-    {
-      label: "Reason",
-      renderCell: (item) => capitalizeFirstLetter(item.reason),
-    },
-    {
-      label: "Status",
-      renderCell: (item) => capitalizeFirstLetter(item.status),
-    },
-  ];
-
   return (
     <>
-      <CompactTable columns={COLUMNS} data={data} theme={theme} />
+      {data && data.length > 0 ? (
+        <div className="my-4 overflow-x-auto">
+          <Table className="mx-auto w-auto">
+            <Table.Head>
+              <Table.HeadCell>Start Date</Table.HeadCell>
+              <Table.HeadCell>Ending Date</Table.HeadCell>
+              <Table.HeadCell>Reason</Table.HeadCell>
+              <Table.HeadCell>Status</Table.HeadCell>
+            </Table.Head>
+            <Table.Body className="divide-y">
+              {data.map((leave) => {
+                return (
+                  <Table.Row
+                    key={leave._id}
+                    className="bg-white dark:border-gray-700 dark:bg-gray-800"
+                  >
+                    <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                      {new Date(leave.startDate).toDateString()}
+                    </Table.Cell>
+                    <Table.Cell>
+                      {new Date(leave.endDate).toDateString()}
+                    </Table.Cell>
+                    <Table.Cell>
+                      {capitalizeFirstLetter(leave.reason)}
+                    </Table.Cell>
+                    <Table.Cell>
+                      {capitalizeFirstLetter(leave.status)}
+                    </Table.Cell>
+                  </Table.Row>
+                );
+              })}
+            </Table.Body>
+          </Table>
+        </div>
+      ) : (
+        <div className="mx-auto w-[80%] py-2 text-center text-xl">
+          No leave record found
+        </div>
+      )}
     </>
   );
 };
