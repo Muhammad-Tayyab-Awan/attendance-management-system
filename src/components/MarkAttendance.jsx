7960eb1361fea90ee9b1446cd09d83846adc82b7
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2025-01-16 21:11:26 +0500

    refactor: integrated tabs in UserDashboard and add SubmitTodayLeave component in MarkAttendance

--- src/components/MarkAttendance.jsx
+++ src/components/MarkAttendance.jsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import attendanceApi from "../api/attendanceApi";
 import toast from "react-hot-toast";
 import { useAttendanceContext } from "../context/AttendanceContext";
+import SubmitTodayLeave from "./SubmitTodayLeave";
 const startHour = import.meta.env.VITE_START_HOUR;
 
 function MarkAttendance() {
@@ -44,48 +45,51 @@ function MarkAttendance() {
   };
 
   return (
-    <div className="mx-auto max-w-md space-y-4 rounded-lg bg-white p-6 shadow-md dark:bg-gray-800">
-      <h1 className="text-xl font-semibold text-blue-600 dark:text-blue-400">
-        Mark Today Attendance
-      </h1>
-      <p className="text-gray-700 dark:text-gray-300">
-        {marked
-          ? status === "absent"
-            ? "You are absent today"
-            : status === "leave"
-              ? "You are on leave today"
-              : `You are ${status} today`
-          : date.getHours() >= startHour - 1 && date.getHours() <= 7
-            ? `Attendance is not marked yet. Please mark your attendance before ${startHour} o'clock`
-            : `You can only mark your attendance from ${startHour - 1} o'clock to ${startHour} o'clock`}
-      </p>
-      <div className="text-gray-700 dark:text-gray-300">
-        <p>
-          <span className="font-medium">Date:</span> {date.toDateString()}
+    <>
+      <div className="mx-auto max-w-md space-y-4 rounded-lg bg-white p-6 shadow-md dark:bg-gray-800">
+        <h1 className="text-xl font-semibold text-blue-600 dark:text-blue-400">
+          Mark Today Attendance
+        </h1>
+        <p className="text-gray-700 dark:text-gray-300">
+          {marked
+            ? status === "absent"
+              ? "You are absent today"
+              : status === "leave"
+                ? "You are on leave today"
+                : `You are ${status} today`
+            : date.getHours() >= startHour - 1 && date.getHours() <= 7
+              ? `Attendance is not marked yet. Please mark your attendance before ${startHour} o'clock`
+              : `You can only mark your attendance from ${startHour - 1} o'clock to ${startHour} o'clock`}
         </p>
-        <p>
-          <span className="font-medium">Time:</span>{" "}
-          {date.toTimeString().split(" ")[0]}
-        </p>
+        <div className="text-gray-700 dark:text-gray-300">
+          <p>
+            <span className="font-medium">Date:</span> {date.toDateString()}
+          </p>
+          <p>
+            <span className="font-medium">Time:</span>{" "}
+            {date.toTimeString().split(" ")[0]}
+          </p>
+        </div>
+        <Button
+          disabled={
+            status ||
+            date.getHours() > startHour ||
+            date.getHours() < startHour - 1
+          }
+          onClick={(e) => {
+            e.preventDefault();
+            markAttendance();
+            setProcessing(true);
+          }}
+          isProcessing={processing}
+          type="submit"
+          color={marked ? "failure" : "success"}
+        >
+          {marked ? "Already Marked" : "Mark Now"}
+        </Button>
       </div>
-      <Button
-        disabled={
-          status ||
-          date.getHours() > startHour ||
-          date.getHours() < startHour - 1
-        }
-        onClick={(e) => {
-          e.preventDefault();
-          markAttendance();
-          setProcessing(true);
-        }}
-        isProcessing={processing}
-        type="submit"
-        color={marked ? "failure" : "success"}
-      >
-        {marked ? "Already Marked" : "Mark Now"}
-      </Button>
-    </div>
+      {!marked && <SubmitTodayLeave />}
+    </>
   );
 }
 
--- src/components/UserDashboard.jsx
+++ src/components/UserDashboard.jsx
@@ -1,3 +1,4 @@
+import { Tabs } from "flowbite-react";
 import { AttendanceProvider } from "../context/AttendanceContext";
 import MarkAttendance from "./MarkAttendance";
 import SubmitLeave from "./SubmitLeave";
@@ -6,14 +7,24 @@ import ViewLeaveUser from "./ViewLeaveUser";
 
 function UserDashboard() {
   return (
-    <div>
+    <>
       <AttendanceProvider>
-        <MarkAttendance />
-        <SubmitLeave />
-        <ViewAttendanceUser />
-        <ViewLeaveUser />
+        <Tabs aria-label="Default tabs" variant="default" className="w-[70vw]">
+          <Tabs.Item title="Mark Attendance" active>
+            <MarkAttendance />
+          </Tabs.Item>
+          <Tabs.Item title="Submit Leave">
+            <SubmitLeave />
+          </Tabs.Item>
+          <Tabs.Item title="Attendance Record">
+            <ViewAttendanceUser />
+          </Tabs.Item>
+          <Tabs.Item title="Leave Record">
+            <ViewLeaveUser />
+          </Tabs.Item>
+        </Tabs>
       </AttendanceProvider>
-    </div>
+    </>
   );
 }
 
